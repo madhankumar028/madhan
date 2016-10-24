830368537cbb69b8dde454744bb73dcd0e25a892
madhankumar028 <madhankumar029@outlook.com>
2016-10-25 00:35:17 +0530

    configured shell script

--- server/runserver.sh
+++ server/runserver.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 echo 'Executing this should run the server at localhost:8000'
-
 echo "Node Modules installation is starting ........"
 npm install
 echo "Node Modules installation is ended"
-echo "Grunt task(jshint) to validate all js files"
+echo "starting Grunt task(jshint) to validate all js files"
 grunt test
 echo "jshint task is completed"
-echo "start node server"
-awhile=3
-sleep $awhile && chrome "http://localhost:8080" &&
+echo "starting node server"
+awhile=5
+sleep $awhile && open chrome "http://localhost:8080"
 grunt server
 #Eg: node server.js
